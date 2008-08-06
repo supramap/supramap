4fc7feb93d1c0e1cb611b42f182b6bc2bbb92097
Jen Chen <jenchen@hucho.(none)>
2008-08-06 09:40:18 -0400

    add log?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = false
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
-config.action_view.cache_template_extensions         = false
+# config.action_view.cache_template_extensions         = false
 
 # care if the mailer can't send
 config.action_mailer.raise_delivery_errors = true
