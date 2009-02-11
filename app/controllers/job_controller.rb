7bc3742a4855a1e2934862e20d07d7ee1e9ec953
Travis Treseder <treseder.2@osu.edu>
2009-02-11 14:32:51 -0500

    Finished job refactoring

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -21,46 +21,12 @@ class JobController < ApplicationController
 
   def create
     @job = Job.new(params[:job])
-    @job.status = "Created"
-    if @job.save
-      
+    if @job.save && (@job.start == 0)
+      flash[:notice] = "Job #{@job.name} successfully created and started."      
     else
-      flash[:notice] = "Job #{@job.name} could not be created."      
-    end
-
-    redirect_to(:controller => "project", :action => "show", :id => @job.project_id)
-  end
-  
-  
-  def create2
-    @sfiles = Sfile.find_all_by_project_id(params[:job][:project_id])
-    @jobs = Job.find_all_by_project_id(params[:job][:project_id])
-    @project = Project.find(params[:job][:project_id])
-    
-    @job = Job.new(params[:job])
-    if @job.save
-      path = "#{FILE_SERVER_ROOT}/#{@project.user.login}/#{@project.id}/#{@job.id}"
-      FileUtils.mkdir(path)
-      FileUtils.chmod_R(0777, path)
-      @sfiles.each do |sfile|
-        if params[:files][sfile.id.to_s] == "1"
-          @job.sfiles << sfile
-        end
-      end
-      # there should be separate actions for different kinds of jobs
-      if(@job.job_type == "fas")
-        @job_status = start_fas_job(@job.id)[0]
-      else
-        @job_status = start_xml_job(@job.id[0])
-      end
-      if @job_status == "0"
-        flash[:notice] = "Job #{@job.name} successfully created and started."
-      else
-        flash[:notice] = "Job #{@job.name} created but cannot start."
-      end
-    else
+      @job.destroy
       flash[:notice] = "Job #{@job.name} could not be created."
     end
-      redirect_to(:controller => "project", :action => "show", :id => @project.id)
+    redirect_to(:controller => "project", :action => "show", :id => @job.project_id)
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -5,10 +5,17 @@ class Job < ActiveRecord::Base
   validates_presence_of :name, :status
   validates_uniqueness_of :name, :scope => :project_id
 
+  attr_reader :path_to_job_dir    
+
+  def initialize
+    super
+    @path_to_job_dir = "#{FILE_SERVER_ROOT}/#{self.project.user.login}/#{self.project_id}/#{self.id}/"
+  end
+
   # create job dir before job is created
   def after_create
     begin 
-      path = get_path
+      path = path_to_job_dir
       logger.error("Creating job dir: #{path}")
       FileUtils.mkdir(path)
       FileUtils.chmod_R(0777, path)
@@ -16,20 +23,22 @@ class Job < ActiveRecord::Base
       logger.error("An error occurred creating job dir for job: #{self.id}")
       return false
     end
-  
   end
 
   # delete job dir after destroyed
   def after_destroy
-    path = get_path
+    path = path_to_job_dir
     FileUtils.rm_r(path) if File.exist?(path)
+
+    if self.status == "Running"
+      self.stop
+    end
   end
 
-  def get_path
-    "#{FILE_SERVER_ROOT}/#{self.project.user.login}/#{self.project_id}/#{self.id}/"
+  def path_to_job_dir
+
   end
   
-  # this is implemented
   def start
     driver = SOAP::WSDLDriverFactory.new(WSDL_URL).create_rpc_driver
     stat = driver.startPOYJob(self.id)
@@ -37,4 +46,8 @@ class Job < ActiveRecord::Base
     return stat
   end
   
+  def stop
+    driver = SOAP::WSDLDriverFactory.new(WSDL_URL).create_rpc_driver
+    return driver.stopJob(self.id)
+  end
 end
